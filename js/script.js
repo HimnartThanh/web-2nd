6ccedbb1c3d3f33f35415a24d583e1b0223eef92
Thanhtran <minhthanhtran14@gmail.com>
2026-06-08 01:44:02 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// Ghi nhận sự kiện khi trang đã tải xong
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("Giao diện Shop Đồ Công Nghệ đã khởi chạy!");
+    
+    // Website được thiết kế tĩnh 100% bằng HTML/CSS để đảm bảo tốc độ tải trang
+    // Nút Zalo đã tích hợp mã tự động điền tin nhắn trong thuộc tính href của HTML.
+});
